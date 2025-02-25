3bfd0e02f74d717cb9ec6265ab090be76b03d952
Mritunjay Pandey <mritunjaypandey.ee@gmail.com>
2025-02-26 03:24:13 +0530

    completed day 27
    Used Axios, https calls using api to print text, show img on html page

--- src/com/mritunjay/Day14__to_27_JavaScript/JavaScript_day14/Day14/app.js
+++ src/com/mritunjay/Day14__to_27_JavaScript/JavaScript_day14/Day14/app.js
@@ -97,14 +97,29 @@ btn.addEventListener("click", async ()=>{
     // console.log("button was clicked");
     let country = document.querySelector("input").value;
     // console.log(country);
-    getColleges(country);
+    let collegesArr = await getColleges(country);
+    // console.log(collegesArr);
+    show(collegesArr);
 })
+
+ function show(collArr){
+    let list = document.querySelector("#list");
+    list.innerText = "";
+    for(col of collArr){
+        console.log(col.name);
+
+        let li = document.createElement("li");
+        li.innerText = col.name;
+        list.appendChild(li);
+    }
+ }
 
 
 async function getColleges(country){
     try{
         let res = await axios.get(url+country);
-        console.log(res.data);
+        // console.log(res.data);
+        return res.data;
     }
     catch(err){
         console.log(err);
