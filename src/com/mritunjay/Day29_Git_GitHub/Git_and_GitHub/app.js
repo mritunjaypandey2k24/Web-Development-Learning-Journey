811f21b07ae4d4ca96cb795489a21a2aeaebb7a8
mritunjaypandey2k24 <mritunjaypandey.ee@gmail.com>
2025-02-28 02:18:23 +0530

    Learning git commands on day29

---
+++ src/com/mritunjay/Day29_Git_GitHub/Git_and_GitHub/app.js
@@ -0,0 +1,37 @@
+//1. Git and GitHub
+
+//What is Git?
+// Free & Open Source Version Control System(VCS)
+// VCS is tools that help to tracks changes in code
+//They help to collaborate in group and help in solving issue
+
+ //What is Github?
+// Website where we host repositories online
+
+// first we add then commit
+
+
+//4 Using Git
+// • Command Line (Most Popular)
+// • IDE I Code Editors (like VSCode)
+// • Graphical User Interface (like GitKraken)
+
+//6 Configuring Git
+// git config --global user.name "My Name"
+// git config --global user.email "someone@email.com"
+
+//7,8  Basic Commands
+// • Clone - Cloning a repository on our local machine
+// • status - displays the state of the code
+// git clone <- some link ->
+// git status
+
+//9  Basic Commands
+// • add - adds new or changed files in your working directory to the Git staging area.
+// git add <- file name ->
+
+// • commit - it is the record of change
+// git commit -m "some message"
+
+// • push - upload local repo content to remote repo
+// git push origin main
